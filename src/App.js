adb43ad4cea8c4341e429dafdfe0999dbd1a176e
Khushi <khushisinha280@gmail.com>
2024-07-23 09:48:01 +0530

    Add all files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+// Importing necessary modules
+import { ToastContainer } from "react-toastify";
+import AddContact from "./components/Contact/AddContact_Form";
+import ContactList from "./components/Contact/ContactList";
+import Navbar from "./components/Navbar/Navbar";
+import { useValues } from "./contactContext";
 
 function App() {
+  // Fetching the context values
+  const{showForm,showContactList,theme}=useValues();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={theme==='light'?"light":"dark"}>
+        <Navbar />
+        <ToastContainer/>
+        {showContactList&&<ContactList />}
+        {showForm&&<AddContact />}
     </div>
   );
 }
